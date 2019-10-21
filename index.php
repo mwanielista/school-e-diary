4c7086f29c5dfd3fafad483bbe965fdff22e9abe
Michał Wanielista <michal@wanielista.eu>
2019-10-21 09:51:17 +0200

    first test commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+    require "header.php";
+?>
+<div class="site-wrapper">
+    <main>
+          <div class="login_notification">
+
+          <?php
+            if(isset($_SESSION['userId'])) {
+                echo "<p>You are logged in</p>";
+                echo '
+                    <style type="text/css">
+                        #login {
+                            visibility: hidden;   
+                        }
+                        #signup {
+                            visibility: hidden;   
+                        }
+                    </style>
+                ';
+            } else{
+                echo "<p>You are logged out</p>";
+                echo '
+                <style type="text/css">
+                        #login {
+                            visibility: visible;   
+                        }
+                        #signup {
+                            visibility: visible;   
+                        }
+                    </style>
+                ';
+            }
+          ?>
+            
+            
+         </div>
+    </main>
+
+<?php
+    require "footer.php";
+?>
