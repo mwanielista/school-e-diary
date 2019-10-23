3e718c519fa573a3e7f6dab2c477d84a2b5d6ba9
Michał Wanielista <mwanielista@o2.pl>
2019-10-23 14:23:10 +0200

    fixed background bug in signup page

--- signup.php
+++ signup.php
@@ -17,20 +17,18 @@
     <div class="signup-page">
         <section class="signup">
             <div class="bg-blur"></div>
-            <div class="signup-dark" id="signup-sect">
-                <form method="post" action="includes/signup.inc.php">
-                    <h2>Zarejestruj się</h2>
-                    <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
-                    <div class="form-group"><input class="form-control" type="text" name="uid" placeholder="username"></div>
-                    <div class="form-group"><input class="form-control" type="text" name="mail" placeholder="Email"></div>
-                    <div class="form-group"><input class="form-control" type="password" name="pwd" placeholder="Password"></div>
-                    <div class="form-group"><input class="form-control" type="password" name="pwd-repeat" placeholder="Repeat password"></div>
-                    <div class="form-group"><input class="form-control" type="text" name="check_sum" placeholder="Auth code"></div>
-                    <div class="form-group"><button class="btn btn-primary btn-block" name="signup-submit" type="submit">Sign up</button></div>
-                </form>
-            </div>
-            
-
+                <div class="signup-dark" id="signup-sect">
+                    <form method="post" action="includes/signup.inc.php">
+                        <h2>Zarejestruj się</h2>
+                        <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
+                        <div class="form-group"><input class="form-control" type="text" name="uid" placeholder="username"></div>
+                        <div class="form-group"><input class="form-control" type="text" name="mail" placeholder="Email"></div>
+                        <div class="form-group"><input class="form-control" type="password" name="pwd" placeholder="Password"></div>
+                        <div class="form-group"><input class="form-control" type="password" name="pwd-repeat" placeholder="Repeat password"></div>
+                        <div class="form-group"><input class="form-control" type="text" name="check_sum" placeholder="Auth code"></div>
+                        <div class="form-group"><button class="btn btn-primary btn-block" name="signup-submit" type="submit">Sign up</button></div>
+                    </form>
+                </div>
             </section>
          </div>
 <?php
